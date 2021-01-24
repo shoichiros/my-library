622919763f676cd0de42a41e28063192f3643cb6
ss <46625673+shoichiros@users.noreply.github.com>
2021-01-24 20:54:35 +0900

    Indent adjustment

--- WP_functions/automatically-link-and-title.php
+++ WP_functions/automatically-link-and-title.php
@@ -6,11 +6,11 @@ function automatically_link_and_title( $atts ) {
       'id' => '',
   ), $atts );
 
-	$href_link = get_the_permalink( $content_atts['id'] );
-	$title = get_the_title( $content_atts['id'] );
-	$link = "<a href=" . $href_link . ">" . $title . "</a>";
+  $href_link = get_the_permalink( $content_atts['id'] );
+  $title = get_the_title( $content_atts['id'] );
+  $link = "<a href=" . $href_link . ">" . $title . "</a>";
 
-	return $link;
+  return $link;
 }
 
 add_shortcode( 'link_auto', 'automatically_link_and_title' );
