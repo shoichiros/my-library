6253d5d1b13cece4eff707c1ad731999f3e645ef
ss <46625673+shoichiros@users.noreply.github.com>
2021-08-26 00:24:15 +0900

    first commit

---
+++ JavaScript/Vue/Components/ContentsList.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="contents-list">
+    <div class="search">
+      <input v-model="searchText" type="search" placeholder="質問タイトル検索...">
+    </div>
+    <div class="content" v-for="content in search" :key="content.title">
+      <span>{{ content.category }}</span>
+      <span>Answer: {{ content.answerCount }}</span>
+      <span>Auther: {{ content.author }}</span>
+      <span>Create date: {{ content.createDate }}</span>
+      <h2>{{ content.title }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContentsList',
+  data() {
+    return {
+      searchText: '',
+      contents: [
+        {
+          title: 'test contents',
+          category: 'IT',
+          answerCount: 2,
+          author: 'Shou',
+          createDate: "2021-01-01"
+        },
+      ]
+    }
+  },
+  computed: {
+    search: function() {
+      return this.contents.filter(content => {
+        return content.title.includes(this.searchText)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.search input {
+  border: 1px solid rgba(0, 0, 0, .2);
+  border-radius: 50px 20px 50px 20px;
+  padding: 10px 20px;
+  width: 500px;
+  outline: none;
+}
+
+.contents-list {
+  color: #666;
+  margin: 100px auto;
+  width: 60%;
+}
+
+.content {
+  border-bottom: 1px solid rgba(0, 0, 0, .2);
+  margin: 20px 0 0 0;
+  padding: 20px;
+  text-align: left;
+  width: 100%;
+  height: 100%;
+}
+
+.content h2 {
+  font-size: 20px;
+  margin: 10px 0 20px 0;
+}
+
+.content span {
+  font-size: 16px;
+  font-weight: 600;
+  margin: 0 20px 0 0;
+}
+</style>
---
+++ JavaScript/Vue/Components/TopHeader.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="top-header">
+    <header class="top-header-contents">
+      <h1>{{ siteTitle }}</h1>
+      <nav class="top-header-nav">
+        <a v-for="list in navLists" :key="list.name" href="">{{ list.name }}</a>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopHeader',
+  props: {
+    siteTitle: String
+  },
+  data() {
+    return {
+      navLists: [
+        { name: 'About' },
+        { name: 'Q&A' },
+        { name: 'Blog' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.top-header {
+  background: rgba(255, 255, 255, .8);
+  position: fixed;
+  width: 100%;
+  height: 80px;
+}
+
+.top-header-contents {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto;
+  width: 90%;
+  height: 100%;
+}
+
+.top-header-contents h1 {
+  color: #555;
+  font-size: 24px;
+}
+
+.top-header-nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.top-header-nav a {
+  color: #555;
+  font-weight: 600;
+  margin: 0 20px;
+  text-decoration: none;
+}
+</style>
