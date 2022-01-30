7df38a055b584ec292dd31f99ac498c3d689d0ef
ss <46625673+shoichiros@users.noreply.github.com>
2022-01-30 22:49:45 +0900

    first commit

---
+++ javascript/vue-components/BaseCountButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="emit('increment')" class="base-count-button">increment</button>
+  <button @click="emit('decrement')" class="base-count-button">decrement</button>
+</template>
+
+<script setup>
+// vue@3.2.29
+const emit = defineEmits(["increment", "decrement"])
+</script>
+
+<style scoped>
+.base-count-button {
+  display: block;
+  background-color: #666;
+  border: none;
+  border-radius: 5px;
+  color: #eee;
+  width: 100%;
+  height: 35px;
+  cursor: pointer;
+}
+</style>
---
+++ javascript/vue-components/BaseInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <input
+    type="text"
+    class="main-base-input"
+    :placeholder="placeholder"
+    :value="modelValue"
+    @input="emit('update:modelValue', $event.target.value)"
+    @change="emit('update:log')"
+  />
+</template>
+
+<script setup>
+defineProps({
+  modelValue: {
+    type: String,
+    required: true
+  },
+  placeholder: {
+    type: String,
+    required: false
+  }
+})
+
+const emit = defineEmits(["update:modelValue", "update:log"])
+</script>
+
+<style scoped>
+.main-base-input {
+  display: block;
+  border: 1px solid #aaa;
+  box-sizing: border-box;
+  padding: 10px 8px;
+  width: 200px;
+}
+</style>
